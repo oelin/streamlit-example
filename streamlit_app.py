d891b6c63dabcd3121c6a01431cc98d4e2aebd2e
kale <42823429+oelin@users.noreply.github.com>
2023-08-30 10:13:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,36 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import openai
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# Set your OpenAI API key here
+openai_api_key = "YOUR_OPENAI_API_KEY"
 
-testing 123
+st.title("Language Learning Chatbot")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+language_prompt = st.text_input("Enter the language you would like to learn:")
+if not language_prompt:
+    st.info("Please enter a language you would like to learn.")
+    st.stop()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+lesson_prompt = f"You have chosen to learn {language_prompt}. Let's start with the basics of {language_prompt} grammar and vocabulary."
 
+st.session_state.messages = [{"role": "assistant", "content": "Welcome to your language learning lesson! I'm here to help you learn."}]
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+if st.session_state.messages[-1]["role"] == "assistant":
+    st.session_state.messages.append({"role": "assistant", "content": lesson_prompt})
 
-    points_per_turn = total_points / num_turns
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Continue the conversation with GPT-3.5-Turbo
+    openai.api_key = openai_api_key
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=st.session_state.messages
+    )
+    assistant_msg = response.choices[0].message
+    st.session_state.messages.append({"role": "assistant", "content": assistant_msg["content"]})
+    st.chat_message("assistant").write(assistant_msg["content"])
